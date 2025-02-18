4260daec1baaecbded15f8510f376033bacfc394
Jeshva Praveen <176747779+Jeshva-Praveen@users.noreply.github.com>
2025-02-18 10:22:38 +0530

    Update and rename system.c to systemcalls1.c

--- Labs/systemcalls1.c
+++ Labs/systemcalls1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<dirent.h>
-#include<stdlib.h>  // Required for exit()
+#include<stdlib.h>
 
 struct dirent *dptr;
 
@@ -9,22 +9,20 @@ int main(int argc, char *argv[])
     char buff[100];
     DIR *dirp;
 
-    // Prompt the user to enter a directory name
+
     printf("Enter Directory name: ");
     scanf("%s", buff);
 
-    // Open the directory
     if ((dirp = opendir(buff)) == NULL) {
         printf("The given directory doesn't exist\n");
         exit(1);
     }
-
-    // Read and print all directory entries
+    printf("Contents of directory %s: \n",buff)
     while ((dptr = readdir(dirp)) != NULL) {
         printf("%s\n", dptr->d_name);
     }
 
-    // Close the directory stream
+ 
     closedir(dirp);
     
     return 0;
